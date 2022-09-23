dd49eb8ad18bed044927eba4a50cabbad60db500
Johanx22x <johanrodsa2210@gmail.com>
2022-09-23 03:46:32 -0600

    Add documentation to jsonm.go

--- internal/jsonm/jsonm.go
+++ internal/jsonm/jsonm.go
@@ -14,63 +14,89 @@ import (
 	"github.com/crawlerclub/ce"
 )
 
+// Save metadata obtained from the most visited websites
+//
+// Receive the number of workers to implement in concurrency
 func SaveMetadata(WORKERS int) {
+    // Create the WaitGroup
     wg := sync.WaitGroup{}
 
+    // Get the stored urls
     idexUrls := csv.GetURLs()
+
+    // Create a list with only the url (exclude the index)
     var urls []string
     for _, val := range idexUrls {
         urls = append(urls, val[1])
     }
 
+    // Create chunk slices with the WORKERS size
     urlList := concurrent.ChunkSlice(urls, WORKERS)
 
+    // Create a buffer to store the channels
     buffers := make([]chan ce.Doc, len(urlList))
 
+    // Iterate over the chunks
     for idx, urls := range urlList {
+        // Create a temporal channel
         tmpchan := make(chan ce.Doc, len(urls))
+
+        // For any single url in the chunk
         for _, url := range urls {
+            // Add a worker
             wg.Add(1)
+            // Get metadata
             go scrap.Metadata(&wg, tmpchan, url)
         }
+        // Add the tmpchan to the buffer
         buffers[idx] = tmpchan
     }
+    // Wait all the workers
     wg.Wait()
 
+    // Create a map to store the obtained metadata
     var metaList = make(map[string]ce.Doc)
 
+    // Iterate over the chunks
     for i := 0; i < len(urlList); i++ {
+        // While the buffer isn't empty
         for len(buffers[i]) > 0 {
+            // Add the information to the metadata map
             response := <-buffers[i]
             metaList[response.Url] = response
         }
     
     }
 
+    // Convert the metadata list to a json format 
     jsonFile, err := json.Marshal(metaList)
     if err != nil {
         log.Fatal(err)
     }
 
+    // Save the metadata obtained in json format
     ioutil.WriteFile("Data/websites-metadata.json", jsonFile, os.ModePerm)
 }
 
+// Load the obtained data from the JSON file
 func LoadMetadata() {
+    // Read the file and error management
     content, err := ioutil.ReadFile("Data/websites-metadata.json")
     if err != nil {
         log.Fatal(err)
     }
 
+    // Create a map for store the content
     payload := make(map[string]ce.Doc)
     err = json.Unmarshal(content, &payload)
-
     if err != nil {
         log.Fatal("Error during Unmarshal(): ", err)
     }
 
+    // TODO: implement data return
     cont := 0
     for _, page := range payload {
-        fmt.Println(cont, page.Url)
+        fmt.Println(cont, page.Url, page.Text)
         cont++
     }
 }
--- internal/scrap/obtainWebsites.go
+++ internal/scrap/obtainWebsites.go
@@ -12,47 +12,72 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// Get metadata from a website
+//
+// Metadata receives a WaitGroup, a channel to send the result and the url of the website
+// Metadata sends the result to the channel, doesn't return any data
 func Metadata(wg *sync.WaitGroup, out chan ce.Doc, url string) {
     defer wg.Done()
 
-    url = fmt.Sprintf("https://%s", url)
+    // Format the url to a valid format
+    url = fmt.Sprintf("https://%s", url) 
 
+    // Response from the website
 	res := dl.DownloadUrl(url)
 
+    // Obtain items from the response
 	items := strings.Split(res.RemoteAddr, ":")
 	ip := ""
 	if len(items) > 0 {
 		ip = items[0]
 	}
+
+    // Parse the content
 	doc := *ce.ParsePro(url, res.Text, ip, true)
 
+    // Send the obtained data to the channel
     out <- doc
 }
 
+// Obtain a CSV table with the most viewed websites in the world
+// 
+// Store the data in "./Data/top-websites.csv"
 func ObtainCSV() {
+    // Make a directory to store the data
     os.Mkdir("Data", os.ModePerm)
 
+    // New file name
     fName := "Data/top-websites.csv"
+    // Create the file
     file, _ := os.Create(fName)
+    // Close the file at the end of the function
     defer file.Close()
 
+    // Create a new csv writer
     writer := csv.NewWriter(file)
+    // Flush the writer at the end of the function
     defer writer.Flush()
 
+    // New colly Collector
     c := colly.NewCollector(
+        // Only search in allowed domains
         colly.AllowedDomains("www.htmlstrip.com"),
     )
 
-    // Find and print all links
+    // Search .row component in the page
     c.OnHTML(".row", func(e *colly.HTMLElement) {
+        // Get the rows
         row := e.ChildText(".col-6")
 
+        // Format the row
         idAndValue := strings.Fields(row)
 
+        // Exclude the first row
         if len(idAndValue) > 1 {
             id := idAndValue[0]
             value := idAndValue[1]
 
+            // Write all the allowed rows in the file
             writer.Write([]string{
                 id,
                 value,
@@ -60,5 +85,6 @@ func ObtainCSV() {
         }
     })
 
+    // Visit the website with the data table 
     c.Visit("https://www.htmlstrip.com/alexa-top-1000-most-visited-websites#")
 }
