f8e80045c8c455d2f5278a6c4333fa9fbd6b9039
Johanx22x <johanrodsa2210@gmail.com>
2022-09-26 11:40:11 -0600

    Create token module

---
+++ internal/token/token.go
@@ -0,0 +1,10 @@
+package token;
+
+import "strings"
+
+func tokenize(text string) (tokenizeText []string) {
+    splitText := strings.Fields(text);
+    // TODO: tokenize text
+    tokenizeText = splitText
+    return 
+}
